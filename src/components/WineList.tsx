147c13cd9f94891f1d3f1676d3585ed454e26f9f
jilink <jimmyfac1@gmail.com>
2023-09-08 16:35:57 +0200

    wine category display one picture

--- src/components/WineList.tsx
+++ src/components/WineList.tsx
@@ -1,60 +1,59 @@
-import { Box, Grid, GridItem, Image } from "@chakra-ui/react";
+import { Box, Grid, GridItem, HStack, Image, Stack, VStack } from "@chakra-ui/react";
 
 const WineList = () => {
- 
-
   const wines = [
-  {
-    id: 1,
-    name: "Chardonnay",
-    image: "/images/wine-bottle.jpg",
-  },
-  {
-    id: 2,
-    name: "Merlot",
-    image: "/images/wine-bottle.jpg",
-  },
-  {
-    id: 3,
-    name: "Pinot Noir",
-    image: "/images/wine-bottle.jpg",
-  },
-  {
-    id: 4,
-    name: "Pinot gris",
-    image: "/images/wine-bottle.jpg",
-  },
-  {
-    id: 5,
-    name: "Cabernet",
-    image: "/images/wine-bottle.jpg",
-  },
-  {
-    id: 6,
-    name: "Syrah",
-    image: "/images/wine-bottle.jpg",
-  },
-  {
-    id: 7,
-    name: "Riesling",
-    image: "/images/wine-bottle.jpg",
-  }
-];
-
+    {
+      id: 1,
+      name: "Chardonnay",
+      image: "/images/wine-bottle.jpg",
+    },
+    {
+      id: 2,
+      name: "Merlot",
+      image: "/images/wine-bottle.jpg",
+    },
+    {
+      id: 3,
+      name: "Pinot Noir",
+      image: "/images/wine-bottle.jpg",
+    },
+    {
+      id: 4,
+      name: "Pinot gris",
+      image: "/images/wine-bottle.jpg",
+    },
+    {
+      id: 5,
+      name: "Cabernet",
+      image: "/images/wine-bottle.jpg",
+    },
+    {
+      id: 6,
+      name: "Syrah",
+      image: "/images/wine-bottle.jpg",
+    },
+    {
+      id: 7,
+      name: "Riesling",
+      image: "/images/wine-bottle.jpg",
+    },
+  ];
 
   return (
-    <Grid templateColumns={["1fr", "1fr 1fr", "1fr 1fr 1fr"]} gap={6}>
-      {wines.map((wine) => (
-        <GridItem key={wine.id}>
-          <Box borderRadius="md" boxShadow="md">
-            <Image src={wine.image} alt={wine.name} />
-            <Box mt={2} textAlign="center">
-              {wine.name}
+    <Stack spacing={6} direction={["column", "row"]} justify="center" w="100%">
+      <Image alignSelf="center" boxSize="30%" src="/images/wine-bottle.jpg" alt="wine" />
+      <Grid justifyItems={["center", "left"]}  templateColumns={"1fr"} gap={6}>
+        {wines.map((wine) => (
+          <GridItem key={wine.id}>
+            <Box borderRadius="md" boxShadow="md">
+              <Box mr="auto" textAlign="center">
+                {wine.name}
+              </Box>
             </Box>
-          </Box>
-        </GridItem>
-      ))}
-    </Grid>
+          </GridItem>
+        ))}
+      </Grid>
+    </Stack>
   );
 };
 
